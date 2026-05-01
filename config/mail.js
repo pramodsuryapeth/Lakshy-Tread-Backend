e95ba997b56462ef83ae89114a1722418287699c
Pramod Suryapeth <pramodsuryapeth8228@gmail.com>
2026-05-01 23:35:26 +0530

    transporter

--- config/mail.js
+++ config/mail.js
@@ -5,13 +5,17 @@ const dns = require("dns");
 dns.setDefaultResultOrder("ipv4first");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,        // 🔥 IMPORTANT
-  secure: false,    // 🔥 IMPORTANT
+  host: "74.125.24.108",
+  port: 587,
+  secure: false,
+  family: 4, // 🔥 FORCE IPv4
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
-  }
+  },
+  connectionTimeout: 20000,
+  greetingTimeout: 20000,
+  socketTimeout: 20000
 });
 
 module.exports = transporter;
